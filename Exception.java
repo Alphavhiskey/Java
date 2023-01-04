1950d39e7557cf9d2a6ae1d0c25122b26987492f
Java <82417634+Alphavhiskey@users.noreply.github.com>
2023-01-04 16:28:52 -0600

    Update Exception.java

--- Exception.java
+++ Exception.java
@@ -1,5 +1,6 @@
 
-	
+	import java.util.Arrays;
+import java.util.Scanner;
 	 
 	public class Exception 
 	{
@@ -23,6 +24,33 @@
 	        {
 	            System.out.print("NullPointerException Caught");
 	        }
+	        
+	        int[] myArray = {897, 56, 78, 90, 12, 123, 75};
+	        System.out.println("Elements in the array are:: ");
+	        System.out.println(Arrays.toString(myArray));
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the index of the required element ::");
+	        try {
+	        int element = sc.nextInt();
+	        System.out.println("Element in the given index is :: "+myArray[element]);
+	        }
+	        catch(ArrayIndexOutOfBoundsException e) {
+	            System.out.println("The index you have entered is invalid");
+	            System.out.println("Please enter an index number between 0 and 6");
+	        	
+	        }
+	        try{  
+	            //code that may raise exception  
+	            int result=800/0;  
+	         }
+	         catch(ArithmeticException e)
+	         {
+	             System.out.println(e);
+	         }  
+	         //rest code of the program   
+	         System.out.println("Program still executes");  
+	        }  
+	
 	    
 	}
-}
+
