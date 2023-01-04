eda2dffba93e9be9cbee8779cd6baf5c1c2d8aa4
Java <82417634+Alphavhiskey@users.noreply.github.com>
2023-01-04 15:47:57 -0600

    Create Exception.java

---
+++ Exception.java
@@ -0,0 +1,28 @@
+
+	
+	 
+	public class Exception 
+	{
+	
+	    public static void main (String[] args)
+	    {
+	        // Initializing String variable with null value
+	        String ptr = null;
+	 
+	        // Checking if ptr.equals null or works fine.
+	        try
+	        {
+	            // This line of code throws NullPointerException
+	            // because ptr is null
+	            if (ptr.equals("Aditya"))
+	                System.out.print("Same");
+	            else
+	                System.out.print("Not Same");
+	        }
+	        catch(NullPointerException e)
+	        {
+	            System.out.print("NullPointerException Caught");
+	        }
+	    
+	}
+}
